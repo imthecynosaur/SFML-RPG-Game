230a07147383532d4474b049516020a8c2c6c6cd
cynosaur <itsjustsina@gmail.com>
2024-02-13 01:09:44 +0330

    animation component added

--- Platformer/Player.cpp
+++ Platformer/Player.cpp
@@ -2,14 +2,18 @@
 
 void Player::initializeComponents()
 {
-	createMovementComponent(1000.f, 8.f, 4.f);
+
 }
 
 Player::Player(float x, float y, sf::Texture& texture)
 {
-	initializeComponents();
 	setTexture(texture);
 	setPosition(x, y);
+
+	createMovementComponent(1000.f, 8.f, 4.f);
+	createAnimationComonent(sprite, texture);
+
+	animationComponent->addAnimation("Idle", 100.f, 0, 0, 4, 0, 128, 128);
 }
 
 Player::~Player()
